0fffc8720b1066813e4ae8df94adafc25090901a
Geoffrey Caven <geoffrey@bravenew.world>
2017-03-20 22:45:10 -0300

    add assn to repo

--- server.js
+++ server.js
@@ -1,7 +1,20 @@
 var express = require('express');
-var bodyParser = require('body-parser')
+var bodyParser = require('body-parser');
+var multer  = require('multer');
+var cloudinary = require('cloudinary');
 var path = require('path');
+
 var app = express();
+var upload = multer({ dest: 'uploads/' })
+
+const uuidV4 = require('uuid/v4');
+var cloud_name = 'dhdze0irb';
+
+cloudinary.config({
+  cloud_name: cloud_name,
+  api_key: '482385852363446',
+  api_secret: 'EJq_0z3xMsJ-fXTRd7Rk1voF3X8'
+});
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -11,36 +24,40 @@ var server = app.listen(8081, function () {
    console.log("Listening on port %s", port);
 });
 
-function convert(unit, value, response) {
-  if (typeof value !== 'number') {
-    return response.status(400).send();
-  }
-  if (unit == 'celcius') {
-    //C to F
-    //Multiply by 9, then divide by 5, then add 32
-    var num = parseFloat(value);
-    num = num*9;
-    num = num/5;
-    num = num+32;
-    return response.send({"unit":"farenheit","value":num});
-  } else if (unit == 'farenheit') {
-    //F to C
-    //Deduct 32, then multiply by 5, then divide by 9
-    var num = parseFloat(value);
-    num = num-32;
-    num = num*5;
-    num = num/9;
-    return response.send({"unit":"celcius","value":num});
-  } else {
-    return response.status(400).send();
-  }
-};
+app.get("/list", function(request, response) {
+  console.log('Listing images...');
+  cloudinary.api.resources(function(result){response.send(result)},{ type: 'upload', 'max_results': 500});
+});
 
-app.post("/", function(request, response) {
-    console.log(request.body);
-    if (request.body.unit === undefined || request.body.value === undefined) {
-      return response.status(400).send();
-    } else {
-      return convert(request.body.unit, request.body.value, response);
-    }
+app.post("/upload", upload.single('file0'), function(request, response) {
+  console.log('Uploading...');
+  console.log(request.file);
+  cloudinary.uploader.upload(
+    request.file.path,
+    function(result) {
+      setTimeout(function() {
+        response.send(result);
+      },1000);
+    },
+    { 'public_id': uuidV4() }
+  );
+});
+
+app.post("/upload_url", function(request, response) {
+  console.log('Grabbing image from URL and uploading');
+  console.log(request.body.imageURL);
+  cloudinary.uploader.upload(
+    request.body.imageURL,
+    function(result) {
+      setTimeout(function() {
+        response.send(result);
+      },1000);
+    },
+    { 'public_id': uuidV4() }
+  );
+});
+
+app.post("/delete", function(request, response) {
+  console.log('Deleting ' + request.body.img + '...');
+  cloudinary.uploader.destroy(request.body.img, function(result) { response.send(result) });
 });
